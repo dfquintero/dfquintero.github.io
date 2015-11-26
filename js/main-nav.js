e89b9c51637a43b8a198e479bc16f455dcccad0b
fredy tovar <contactsaliens@gmail.com>
2015-11-26 12:59:27 -0500

    portfolio gallery
    realizacion galeria de portafolio

---
+++ js/main-nav.js
@@ -0,0 +1,18 @@
+ 
+  $(window).load(function(){
+    
+    $('.main-nav li a').bind('click',function(event){
+      var $anchor = $(this);
+      
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 102
+      }, 1500,'easeInOutExpo');
+      /*
+      if you don't want to use the easing effects:
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1000);
+      */
+      event.preventDefault();
+    });
+  })
---
+++ js/script.js
@@ -0,0 +1,12 @@
+ wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100
+      }
+    );
+    wow.init();
+    document.getElementById('').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
