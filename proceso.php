e3b4fd69857f3d6429d40bdaaca7afbdccadd62f
fredy tovar <contactsaliens@gmail.com>
2016-01-24 12:47:42 -0500

    test1
    test1

--- proceso.php
+++ proceso.php
@@ -7,7 +7,7 @@ $mensaje = $_POST['mensaje'];
 $dest = "tovar_fredy@hotmail.com"; 
  
 // Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
-$headers = "From: $nombre $email\r\n";
+$headers .= "From: $nombre $email\r\n";
 $headers .= "X-Mailer: PHP5\n";
 $headers .= 'MIME-Version: 1.0' . "\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
