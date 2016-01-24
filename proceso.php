182cbbb829c0fbdc4bd9aa48fe6d2b45aee00bce
fredy tovar <contactsaliens@gmail.com>
2016-01-24 13:29:34 -0500

    test3
    tst3

--- proceso.php
+++ proceso.php
@@ -11,9 +11,7 @@ $dest = "tovar_fredy@hotmail.com";
 //$headers .= "X-Mailer: PHP5\n";
 //$headers .= 'MIME-Version: 1.0' . "\n";
 //$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
+
  
 // Aqui definimos el asunto y armamos el cuerpo del mensaje
 $asunto = "Contacto";
@@ -22,7 +20,7 @@ $cuerpo .= "<strong>Email:</strong> ".$email."<br>";
 $cuerpo .= "<strong>Mensaje:</strong> ".$mensaje;
  
 // Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
-if($nombre != '' && $email != '' && $mensaje != ''){
-    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR!
-}
+
+    mail($dest,$asunto,$cuerpo,''); //ENVIAR!
+
 ?>
