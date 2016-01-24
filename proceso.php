044665bfe39752257e7a274b3eb0f925b17b8be8
fredy tovar <contactsaliens@gmail.com>
2016-01-24 17:15:22 -0500

    mail
    mail

--- proceso.php
+++ proceso.php
@@ -7,11 +7,11 @@ $mensaje = $_POST['mensaje'];
 $dest = "tovar_fredy@hotmail.com"; 
  
 // Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
-//$headers .= "From: $nombre $email\r\n";
-//$headers .= "X-Mailer: PHP5\n";
-//$headers .= 'MIME-Version: 1.0' . "\n";
-//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
+$headers = "From: $nombre $email\r\n";
+$headers .= "X-Mailer: PHP5\n";
+$headers .= 'MIME-Version: 1.0' . "\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	
  
 // Aqui definimos el asunto y armamos el cuerpo del mensaje
 $asunto = "Contacto";
@@ -21,6 +21,6 @@ $cuerpo .= "<strong>Mensaje:</strong> ".$mensaje;
  
 // Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
 
-    mail($dest,$asunto,$cuerpo,''); //ENVIAR!
+    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR!
 
 ?>
